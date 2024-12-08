c45f1e9196c60ba0e7a5414a108902c87539a564
JohnCarlo <2213473@slu.edu.ph>
2024-12-09 04:13:43 +0800

    Created display board function

--- src/tictactoe.cpp
+++ src/tictactoe.cpp
@@ -1,1 +1,15 @@
-// TicTacToe
+// TicTacToe Game
+#include <iostream>
+using namespace std;
+
+// Function to display the Tic-Tac-Toe board
+void displayGrid(char grid[3][3]) {
+    cout << "-------------\n";
+    for (int row = 0; row < 3; row++) {
+        cout << "| ";
+        for (int col = 0; col < 3; col++) {
+            cout << grid[row][col] << " | ";
+        }
+        cout << "\n-------------\n";
+    }
+}
