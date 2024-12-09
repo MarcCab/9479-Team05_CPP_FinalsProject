5a06a79145c02d38901d2f65df8415d8ff0af49c
JOHNY JAMES CHUMMAC <2224560@slu.edu.ph>
2024-12-09 18:57:22 +0800

    added Hangman class declaration

---
+++ src/hangman.h
@@ -0,0 +1,16 @@
+#ifndef HANGMAN_H
+#define HANGMAN_H
+
+#include <string>
+#include <vector>
+
+class HangmanGame {
+public:
+    // Constructor to initialize the game
+    HangmanGame();
+
+    // Method to start the game
+    void play();
+};
+
+#endif
