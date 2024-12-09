fcd2d71cd707f40a1bb0b8d7134411cccad37558
Jermaine <2226003@slu.edu.ph>
2024-12-09 18:18:37 +0800

    Added playTicTacToe function and modified tictactoe.h to share the function to other classes.

--- src/tictactoe.cpp
+++ src/tictactoe.cpp
@@ -13,3 +13,44 @@ void displayGrid(char grid[3][3]) {
         cout << "\n-------------\n";
     }
 }
+
+
+// Function to play Tic-Tac-Toe
+void playTicTacToe() {
+    char board[3][3] = { 
+                        { ' ', ' ', ' ' }, 
+                        { ' ', ' ', ' ' }, 
+                        { ' ', ' ', ' ' } 
+                       };
+    char player = 'X';
+    int row, col, turn;
+
+    cout << "Welcome to Tic-Tac-Toe!\n";
+
+    for (turn = 0; turn < 9; turn++) {
+        displayGrid(board);
+        while (true) {
+            cout << "Player " << player << ", enter row (0-2) and column (0-2): ";
+            cin >> row >> col;
+
+            if (row < 0 || row > 2 || col < 0 || col > 2 || board[row][col] != ' ') {
+                cout << "Invalid move. Try again.\n";
+            } else {
+                break;
+            }
+        }
+        board[row][col] = player;
+
+        if (checkWin(board, player)) {
+            displayGrid(board);
+            cout << "Player " << player << " wins!\n";
+            return;
+        }
+
+        player = (player == 'X') ? 'O' : 'X';
+    }
+
+    displayGrid(board);
+    cout << "It's a draw!\n";
+}
+
