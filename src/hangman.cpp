e9e2b5aead9e87f19d8f4be1c2d7775cf6b0b8ef
aldwyn <2224186@slu.edu.ph>
2024-12-09 20:48:11 +0800

    add function to start the hangman game

--- src/hangman.cpp
+++ src/hangman.cpp
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
 #include "hangman.h"
 
 using namespace std;
@@ -10,4 +11,42 @@ HangmanGame::HangmanGame() {
     secretWord = getRandomWord();
     currentWord = string(secretWord.length(), '_');
     attemptsLeft = MAX_ATTEMPTS;
+}
+
+// Method to start the game
+void HangmanGame::play() {
+    cout << "Welcome to Hangman!" << endl;
+    cout << "Category: Fruits" << endl;
+    cout << "You have " << attemptsLeft << " attempts to guess the fruit name." << endl;
+
+    while (attemptsLeft > 0) {
+        char guess;
+        cout << "Guess a letter: ";
+        cin >> guess;
+
+        if (isalpha(guess)) {
+            guess = tolower(guess);
+            if (alreadyGuessed(guess)) {
+                cout << "You've already guessed that letter." << endl;
+            } else {
+                bool correctGuess = updateCurrentWord(guess);
+                if (correctGuess) {
+                    cout << "Good guess!" << endl;
+                    if (currentWord == secretWord) {
+                        cout << "Congratulations! You guessed the word: " << endl;
+                        return;
+                    }
+                } else {
+                    cout << "Incorrect guess." << endl;
+                    attemptsLeft--;
+                }
+            }
+        } else {
+            cout << "Please enter a valid letter." << endl;
+        }
+    }
+
+    if (attemptsLeft == 0) {
+        cout << "You've run out of attempts. The word was: "  << endl;
+    }
 }
